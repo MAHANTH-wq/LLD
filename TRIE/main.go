38c2d27fb0d9f0120eaa84d528fa6941af28a6b6
MAHANTH-wq <vallulrimahanthkumar@gmail.com>
2025-08-24 17:14:39 +0530

    Word TRIE

--- TRIE/main.go
+++ TRIE/main.go
@@ -5,21 +5,33 @@ import "fmt"
 func main() {
 
 	trieData := NewTRIE()
+	trieData.Insert("a")
+	trieData.Insert("ap")
 	trieData.Insert("app")
+	trieData.Insert("app")
+	trieData.Insert("appl")
+	trieData.Insert("apple")
 	trieData.Insert("apples")
+	trieData.Insert("apples")
+	trieData.Insert("apples")
+	trieData.Insert("o")
+	trieData.Insert("or")
+	trieData.Insert("ora")
+	trieData.Insert("oran")
+	trieData.Insert("orang")
+	trieData.Insert("orange")
+	trieData.Insert("orange")
 	trieData.Insert("leo")
 
-	if trieData.Search("apple") {
-		fmt.Println("Word apple exist in trie")
-	} else {
-		fmt.Println("Word apple does not exist in trie")
-	}
-
-	if trieData.StartsWith("apple") {
-		fmt.Println("There are words that start with apple")
-	} else {
-		fmt.Println("There are no words that start with apple")
-	}
-
+	fmt.Println("Number of words 'apples' inserted", trieData.Search("apples"))
+	fmt.Println("Number of word with 'app' as prefix", trieData.StartsWith("app"))
 	fmt.Println("Word starts with 'leos' ", trieData.StartsWith("leos"))
+
+	result := trieData.LongestCompleteString(trieData.root)
+	fmt.Println("Longest Complete String", result)
+
+	count := CountNoOfDistinctSubStrings("apple")
+	fmt.Println("No of Distinct Substrings of apple ", count)
+	count = CountNoOfDistinctSubStrings("ababd")
+	fmt.Println("No of Distinct Substrings of ababd ", count)
 }
--- TRIE/trie.go
+++ TRIE/trie.go
@@ -1,8 +1,9 @@
 package main
 
 type Node struct {
-	links [26]*Node
-	flag  bool
+	links           [26]*Node
+	endsWithCount   int
+	prefixWithCount int
 }
 
 func (n *Node) ContainsKey(w rune) bool {
@@ -24,7 +25,8 @@ func (n *Node) Put(w rune) {
 
 func newNode() *Node {
 	return &Node{
-		flag: false,
+		endsWithCount:   0,
+		prefixWithCount: 0,
 	}
 }
 
@@ -49,32 +51,67 @@ func (t *TRIE) Insert(word string) {
 			currentNode.Put(rune(word[i]))
 		}
 		currentNode = currentNode.Get(rune(word[i]))
+		currentNode.prefixWithCount++
 	}
-	currentNode.flag = true
+	currentNode.endsWithCount++
 }
 
-func (t *TRIE) Search(word string) bool {
+func (t *TRIE) Search(word string) int {
 	currentNode := t.root
 	n := len(word)
 	for i := 0; i < n; i++ {
 		if !currentNode.ContainsKey(rune(word[i])) {
-			return false
+			return 0
 		}
 		currentNode = currentNode.Get(rune(word[i]))
 	}
-	return currentNode.flag
+	return currentNode.endsWithCount
 }
 
-func (t *TRIE) StartsWith(word string) bool {
+func (t *TRIE) StartsWith(word string) int {
 
 	currentNode := t.root
 	n := len(word)
 
 	for i := 0; i < n; i++ {
 		if !currentNode.ContainsKey(rune(word[i])) {
-			return false
+			return 0
 		}
 		currentNode = currentNode.Get(rune(word[i]))
 	}
-	return true
+	return currentNode.prefixWithCount
+}
+
+func (t *TRIE) LongestCompleteString(root *Node) string {
+
+	maxString := ""
+	for index, value := range root.links {
+		if value != nil && value.endsWithCount > 0 {
+			ll := t.LongestCompleteString(value)
+			if len(ll) > len(maxString) || maxString == "" {
+				maxString = string(rune('a'+index)) + ll
+			}
+		}
+	}
+	return maxString
+}
+
+func CountNoOfDistinctSubStrings(word string) int {
+
+	trie := NewTRIE()
+	n := len(word)
+	count := 0
+	for i := 0; i < n; i++ {
+		currentNode := trie.root
+		for j := i; j < n; j++ {
+			if !currentNode.ContainsKey(rune(word[j])) {
+				count++
+				currentNode.Put(rune(word[j]))
+			}
+			currentNode = currentNode.Get(rune(word[j]))
+		}
+	}
+
+	return (count + 1)
+
 }
